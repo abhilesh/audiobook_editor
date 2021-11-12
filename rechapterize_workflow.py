b9e13c752885a528529a7f6ebb096af3423cad8b
Abhilesh Dhawanjewar <abhilesh7@gmail.com>
2021-11-12 12:59:20 -0600

    Temp: Disabled m4b existence check

--- rechapterize_workflow.py
+++ rechapterize_workflow.py
@@ -15,8 +15,8 @@ print(f"Step 1: Merging mp3 files into m4b file .....")
 # Merge the mp3 files into a single m4b file
 
 # Check if m4b versions of the file exist
-if not filename.with_suffix('.m4b'):
-	subprocess.run(['m4b-tool', 'merge', filename,
+#if not filename.with_suffix('.m4b'):
+subprocess.run(['m4b-tool', 'merge', filename,
 					f"--output-file={filename.with_suffix('.m4b')}"])
 
 print(f"Step 2: Converting m4b file to wav file .....")
