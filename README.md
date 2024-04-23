db66b71b27dbc92b7be077ec98dd4389d713000a
Abhilesh Dhawanjewar <abhilesh7@gmail.com>
2024-04-23 09:14:23 +0100

    Update syntax Add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+Workflow to process audiobooks
+==============================
+
+This workflow is a barebones pipeline to process audiobooks. I run it on MacOS, but it should run on Linux as well. It uses the following tools:
+
+## Dependencies
+- [m4b-tool](https://github.com/sandreas/m4b-tool)
+- [ffmpeg](https://ffmpeg.org/)
+- [vosk-api](https://github.com/alphacep/vosk-api/tree/a120d06f6b92ad41d0af8c2dc4ec437ad810281f)
+- [ebooklib](https://github.com/aerkalov/ebooklib)
+- [srt](https://github.com/cdown/srt)
+- [fuzzywuzzy](https://github.com/seatgeek/fuzzywuzzy)
+- [num2words](https://github.com/savoirfairelinux/num2words)
--- rechapterize_workflow.py
+++ rechapterize_workflow.py
@@ -1,39 +1,56 @@
 #!/usr/bin/env python3
-
+# %%
 import sys
 import subprocess
 from pathlib import Path
 
 filename = Path(sys.argv[1])
 
-vosk_srt_path = Path.cwd() / 'vosk-api' / 'python' / 'example'
+vosk_srt_path = Path.cwd() / "vosk-api" / "python" / "example"
 
 print(f"Working on rechapterizing {filename}")
 
 print(f"Step 1: Merging mp3 files into m4b file .....")
 
+# %%
+
 # Merge the mp3 files into a single m4b file
 
 # Check if m4b versions of the file exist
-#if not filename.with_suffix('.m4b'):
-subprocess.run(['m4b-tool', 'merge', filename,
-					f"--output-file={filename.with_suffix('.m4b')}"])
+if not filename.with_suffix(".m4b").exists():
+    subprocess.run(
+        ["m4b-tool", "merge", filename, f"--output-file={filename.with_suffix('.m4b')}"]
+    )
 
 print(f"Step 2: Converting m4b file to wav file .....")
 
 # Convert the m4b file to wav
-subprocess.run(['ffmpeg', '-i', 
-				f"{filename.with_suffix('.m4b')}", 
-				'-ar', '16000',
-				'-ac', '1',
-				f"{filename.with_suffix('.wav')}"])
+subprocess.run(
+    [
+        "ffmpeg",
+        "-i",
+        f"{filename.with_suffix('.m4b')}",
+        "-ar",
+        "16000",
+        "-ac",
+        "1",
+        f"{filename.with_suffix('.wav')}",
+    ]
+)
 
 print(f"Step 3: Generating speech-to-text maps for wav file .....")
 
-if not filename.with_suffix('.srt').is_file():
-    srt_outfile = open(filename.with_suffix('.srt'), 'w')
-    subprocess.call(['python', f"{vosk_srt_path / 'test_srt.py'}", f"{filename.with_suffix('.wav')}"], stdout=srt_outfile)
-
+if not filename.with_suffix(".srt").is_file():
+    srt_outfile = open(filename.with_suffix(".srt"), "w")
+    subprocess.call(
+        [
+            "python",
+            f"{'generate_srt.py'}",
+            f"{filename.with_suffix('.wav')}",
+        ],
+        stdout=srt_outfile,
+    )
+# %%
 print(f"Step 4: Identifying chapter timestamps in the audiobook .....")
 
-subprocess.call(['python', 'map_srt_to_epub.py', f"{filename}"])
+subprocess.call(["python", "map_srt_to_epub.py", f"{filename}"])
